ca44d87f04399b2b707262dad3927edf404e600d
Yanos <santiago.yanos@gmail.com>
2022-10-10 19:30:48 -0300

    First Commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express, { application } from "express";
+import morgan from "morgan";
+import path from "path";
+import * as url from "url";
+import mailRoute from "./routes/mail.js";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const app = express();
+
+app.use(express.static(path.join(__dirname + "public")));
+app.use(express.json());
+app.use(morgan("dev"));
+
+// app.use("/mail", mailRoute);
+// app.use("/home", (req, res) => {
+//   res.send("Home");
+// });
+
+app.use("*", (req, res) => {
+  res.redirect("/home");
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-import express, { application } from "express"
-import morgan from "morgan"
-import path from "path"
-import * as url from "url"
-import mailRoute from "./routes/mail.js"
-
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-
-const app = express()
-
-app.use(express.static(path.join(__dirname + "public")))
-app.use(express.json())
-app.use(morgan("dev"))
-
-app.use("/mail", mailRoute)
-app.get("/home", (req, res) => { res.send("Home") })
-
-app.use("*", (req, res) => {
-    res.redirect("/home")
-})
+import app from "./app.js";
 
 app.listen(3000, () => {
-    console.log("server on port 3000")
-})
+  console.log("server on port 3000");
+});
